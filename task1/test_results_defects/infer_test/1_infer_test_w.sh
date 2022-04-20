fb06b7ba22204b1a35b39aa43471b07ac25270e1
HangyuanYang <690094231@qq.com>
2022-04-21 01:17:04 +0800

    add sloc (cloc)

--- task1/test_results_defects/infer_test/1_infer_test_w.sh
+++ task1/test_results_defects/infer_test/1_infer_test_w.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 project_path=$(cd `dirname $0`; cd ../../01.w_Defects; pwd)
 # echo $project_path
-file_name="buffer_overrun_dynamic"
+file_name="data_lost"
 if [ -d $file_name ];then
   rm -rf $file_name
 fi
 # specify one c file, run infer
-infer run --bufferoverrun --disable-issue-type DEAD_STORE --disable-issue-type UNINITIALIZED_VALUE --disable-issue-type NULL_DEREFERENCE -o $file_name -- clang -c $project_path/$file_name.c
+infer run --bufferoverrun -o $file_name -- clang -c $project_path/$file_name.c
 
 if [ -f "$file_name.o" ];then
   rm -f "$file_name.o"
